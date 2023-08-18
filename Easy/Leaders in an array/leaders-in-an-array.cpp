0f0dea86d905c1a598710625b33f04bd2ca2057f
Himanshu2126 <93526524+Himanshu2126@users.noreply.github.com>
2023-08-18 22:29:04 +0530

    Updated Solution - GfG to GitHub

--- Easy/Leaders in an array/leaders-in-an-array.cpp
+++ Easy/Leaders in an array/leaders-in-an-array.cpp
@@ -7,26 +7,23 @@ using namespace std;
 
 // } Driver Code Ends
     
-
 class Solution{
     //Function to find the leaders in the array.
     public:
     vector<int> leaders(int a[], int n){
         // Code here
-        vector<int> res;
-        int last=a[n-1];
-        res.push_back(a[n-1]);
-        for(int i=n-2;i>=0;i--)
-        {
-            if(a[i]>=last)
-            {
-                last=a[i];
-                res.push_back(a[i]);
-            }
-           
+        vector<int> ans;
+        ans.push_back(a[n-1]);
+        
+        int maxi = a[n-1];
+        for(int i=n-2;i>=0;i--){
+            if(a[i]>=maxi){
+                ans.push_back(a[i]);
+                maxi = max(maxi,a[i]);
+            } 
         }
-        reverse(res.begin(),res.end());
-        return res;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
 
